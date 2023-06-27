21c03f53f774ae1cac10176db1e75f74d69c953e
Martin Robinson <mrobinson@igalia.com>
2023-06-27 09:29:38 +0200

    Adapt Servo to new WebRender

--- components/webgpu/lib.rs
+++ components/webgpu/lib.rs
@@ -27,10 +27,8 @@ use std::rc::Rc;
 use std::slice;
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
-use webrender_api::{
-    DirtyRect, DocumentId, ExternalImageId, ImageData, ImageDescriptor, ImageKey, RenderApi,
-    RenderApiSender, Transaction,
-};
+use webrender::{RenderApi, RenderApiSender, Transaction};
+use webrender_api::{DirtyRect, DocumentId, ExternalImageId, ImageData, ImageDescriptor, ImageKey};
 use webrender_traits::{
     WebrenderExternalImageApi, WebrenderExternalImageRegistry, WebrenderImageHandlerType,
     WebrenderImageSource,
